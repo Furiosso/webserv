432687a0142eed3597aff41dc986ee5f58d882c0
Pablo Vicente Hernandez <pavicent@c1r16s5.42madrid.com>
2026-02-13 18:54:11 +0100

    Switch fixed

--- srcs/Parser.cpp
+++ srcs/Parser.cpp
@@ -14,8 +14,9 @@ void    Parser::rmComments(std::ifstream& config_file)
             if (pos != std::string::npos)
                 line = line.substr(0, pos);
             std::string cleanLine = rtrim(line); //desarrollar eliminar espacios
+            cleanLine = trim(cleanLine); //desarrollar eliminar espacios
             if (!cleanLine.empty())
-            this->_config_file = this->_config_file + cleanLine + " ";
+                this->_config_file += cleanLine + " ";
         }
     }
 }
@@ -30,7 +31,8 @@ Parser::Parser(const char* in_file)
     this->tokenize();
     for (size_t i = 0; i < this->_tokens.size(); i++)
        std::cout << this->_tokens[i] << std::endl;
-       //std::cout << this->_config_file[i] << std::endl;
+    /*for (size_t i = 0; i < this->_config_file.size(); i++)
+       std::cout << this->_config_file[i];*/
        
     config_file.close();
 }
@@ -41,28 +43,50 @@ Parser::~Parser()
 void    Parser::tokenize()
 {
     std::string token;
-    std::string line;
-    for (size_t i = 0; this->_config_file[i];++i)
+    for (size_t i = 0; i < this->_config_file.size();++i)
     {
-        switch(this->_config_file[i])
+        char c = this->_config_file[i];
+        switch(c)
         {
             case '{':
+                if (!token.empty())
+                {
+                    _tokens.push_back(token);
+                    token.clear();
+                }
                 _tokens.push_back("{");
                 break ;
             case '}':
-                _tokens.push_back(token);
-                token.clear();
+                if (!token.empty())
+                {
+                    _tokens.push_back(token);
+                    token.clear();
+                }
                 _tokens.push_back("}");
                 break;
             case ';':
-                _tokens.push_back(token);
-                token.clear();
+                if (!token.empty())
+                {
+                    _tokens.push_back(token);
+                    token.clear();
+                }
                 _tokens.push_back(";");
+                break ;
             case ' ':
-                _tokens.push_back(token);
-                token.clear();
+            case '\t':
+            /*case '\n':
+            case '\r':*/
+                if (!token.empty())
+                {
+                    _tokens.push_back(token);
+                    token.clear();
+                }
+                break;
             default:
-                token.push_back(this->_config_file[i]);
+                token.push_back(c);
+                break;
         }
     }
+    if (!token.empty())
+        _tokens.push_back(token);
 }
